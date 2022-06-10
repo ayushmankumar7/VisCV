61adf95aac64d31b9a8a4cee55de00b3ca685cd1
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-06-10 11:44:16 +0530

    vector inherited

--- src/simulation/particle.py
+++ src/simulation/particle.py
@@ -1,14 +1,25 @@
 import cv2
 
-class Particle:
+from vector import Vector
+
+
+class Particle(Vector):
     """The Particle is by default a Circle
     With default thickness or Radius = 20
     """
 
-    def __init__(self,thickness = 20, color = (255, 255, 255)): 
+    def __init__(self,xv = 0, yv = 0, thickness = 20, color = (255, 255, 255)): 
+        """ 
+        xv: X Velocity
+        yv: Y Valocity
+
+        """
+        super(Particle, self).__init__( xv,yv )
         self.thickness = thickness
         self.color = color
-        
+        self.xv = xv
+        self.yv = yv
+
     def draw(self):
         cv2.circle(self.img, self.center, self.thickness, self.color, -1)
     
