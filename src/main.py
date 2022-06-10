8e0eb3202717cddacdde279e5d5704f9a73d0558
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-06-10 11:44:32 +0530

    vector added

--- src/main.py
+++ src/main.py
@@ -1,43 +1,39 @@
 import numpy as np 
 import cv2 
 
-#from visualization.histogram import Histogram
 from simulation.particle import Particle
+
+from vector import Vector
 
 initialised = False
 
-i = 10
-j = 300
-s = 0
 
-state = []
+state = [1]
+width = 600
+height = 600
+
+loc = Vector(100, 100)
+vel = Vector(1, 3)
 
 while True:
     if len(state) > 0:
-        a = np.zeros((600, 600, 3))
-        
-        a = cv2.addWeighted(a, 1.0, state[-1], 0.5, 0.0)
-        if i < 500 and s == 0:
-            i += 4
-    
-        elif i < 10:    
-            s = 0
-        else:
-            s = 1
-            if s == 1:
-                i -= 4
+        a = np.zeros((width, height, 3))   
+        loc = loc + vel
+   
+        if (loc.x > width) or (loc.x < 0):
+            vel.x *= -1 
+        if (loc.y > height) or (loc.y < 0):
+            vel.y *= -1
 
+        
+        
         if not initialised:
             p = Particle()
             initialised = True
-        p(a, (i, j))
-        if len(state) > 7:
-            state.pop(0)
-        state.append(a)
+        p(a, (loc.x, loc.y))
         
         cv2.imshow("Plot", a)
         if cv2.waitKey(10) == 27:
             break
-    else:
-        state.append(np.zeros((600, 600, 3)))
+
 cv2.destroyAllWindows()
---
+++ src/vector.py
@@ -0,0 +1,29 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y 
+    
+    def __add__(self, vector):
+        """ 
+        
+        v = v1 + v2
+        v_x = v1_x + v2_x
+        v_y = v1_y + v2_y
+
+        return v
+        """
+
+        if isinstance(vector, type(self)):
+            return Vector(*(x + y for x, y in zip((self.x, self.y), (vector.x, vector.y))))
+        else:
+            return "Please add a Vector!"
+    
+    def __repr__(self):
+        return f"Vector at: ({self.x}, {self.y})"
+
+if __name__ == "__main__":
+    loc = Vector(9, 0)
+    vel = Vector(1, 3)
+    print(vel)
+    print(loc)
+    print(vel + loc)
